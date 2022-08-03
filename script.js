7be2ed8a216179ff62618faf9270e5c69b65c493
balajcosmin <balajcosmin58@yahoo.com>
2022-08-03 07:41:00 +0300

    added read more button

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ const burgerMenu = document.querySelector(".nav");
 burgerMenu.addEventListener("click", function () {
   burgerMenu.classList.toggle("active");
 });
+
+function readMore() {
+	var dots = document.getElementById("dots");
+	var moreText = document.getElementById("more");
+	var btnText = document.getElementById("myBtn");
+  
+	if (dots.style.display === "none") {
+	  dots.style.display = "block";
+	  btnText.innerHTML = "Read more";
+	  moreText.style.display = "none";
+	} else {
+	  dots.style.display = "none";
+	  btnText.innerHTML = "Read less";
+	  moreText.style.display = "block";
+	}
+  }
